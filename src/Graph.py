15706fcf3aef78a50b38cc1f5c8872d5579e25e4
Nicolas Leão <niicolasleao@gmail.com>
2019-03-27 18:37:14 -0300

    Fixed identation issue

--- src/Graph.py
+++ src/Graph.py
@@ -6,7 +6,7 @@ class Graph:
 		self.directed = directed
 		self.addConnections(connections)
 
-	# Generate the graph dictionary based on the array of touples that represent every edge in the graph.
+	# Generate the graph dictionary based on the array of touples that # represent every edge in the graph.
 	# Example: [(1, 2), (1, 3), (3, 4), (2, 4)] 
 	def addConnections(self, connections):
 		for a, b in connections:
@@ -14,33 +14,32 @@ class Graph:
 
 	# Add an edge to the dictionary
 	def add(self, a, b):
-        self.graph[a].append(b)
+		self.graph[a].add(b)
+		if (not self.directed):
+			self.graph[b].add(a)
 
-        if not self.directed:
-            self.graph[b].append(a)
+	# Remove an edge and all it's references from a dictionary
+	def remove(self, a):
+		for n, ref in self.graph.iteritems():
+			try:
+				ref.remove(a)
+			except KeyError:
+				pass
+		try:
+			del self.graph[a]
+		except KeyError:
+			pass
 
-    # Remove an edge and all it's references from a dictionary
-    def remove(self, a):
-        for n, ref in self.graph.iteritems():
-            try:
-                ref.remove(a)
-            except KeyError:
-                pass
-        try:
-            del self.graph[a]
-        except KeyError:
-            pass
-
-    # Find the shortest path between two nodes 
+	# Find the shortest path between two nodes 
 	def shortest_path(self, start, end, path =[]): 
-        path = path + [start] 
-        if start == end: 
-            return path 
-        shortest = None
-        for node in self.graph[start]: 
-            if node not in path: 
-                newpath = find_shortest_path(self.graph, node, end, path) 
-                if newpath: 
-                    if not shortest or len(newpath) < len(shortest): 
-                        shortest = newpath 
-        return shortest 
+		path = path + [start] 
+		if start == end: 
+			return path 
+		shortest = None
+		for node in self.graph[start]: 
+			if node not in path: 
+				newpath = find_shortest_path(self.graph, node, end, path) 
+				if newpath: 
+					if not shortest or len(newpath) < len(shortest): 
+						shortest = newpath 
+		return shortest
