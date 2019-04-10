da3d138c4111aee7a9c51e31ea4f4ef10946a3e9
Nicolas Leão <niicolasleao@gmail.com>
2019-04-10 10:21:57 -0300

    License added

--- src/remove_orphans.py
+++ src/remove_orphans.py
@@ -1,3 +1,18 @@
+# Orphan package finder, written by Nicolas Leão
+
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from libpath import *
 import os
 
@@ -35,5 +50,3 @@ def remove_orphans():
 
     print("Orphans: ", new_orphans)
     return new_orphans
-
-remove_orphans()
