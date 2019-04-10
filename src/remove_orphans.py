a350483eca1e29d89bf21757846143f07f897503
Nicolas Leão <niicolasleao@gmail.com>
2019-04-10 13:10:50 +0000

    Orphan removal updated

--- src/remove_orphans.py
+++ src/remove_orphans.py
@@ -1,38 +1,39 @@
+from libpath import *
 import os
 
-def remove_orphans(path="."):
-        # read all files in 'path'
-        files = os.listdir(path)
-        orphans = []
+manifest_path = home+"/.mpu/manifests/"
+world_path = home+"/.mpu/world"
 
-        # read all manifest files from folder
-        for file in files:
-                if(file.endswith(".manifest")):
-                        orphans.append(file)
+def remove_orphans():
+    # Add all installed packages to initial orphan list
+    orphans = []
+    with open(world_path, "r") as file:
+        orphans = file.read().splitlines()
 
+    new_orphans = orphans.copy()
+    for file in orphans:
+            # read all dependencies in manifest file and remove them from 'orphans' list 
+            # to ensure no package will lose functionality after orphan removal
+            open_file = open(manifest_path+"/"+file, "r")
+            deps = open_file.read().splitlines()
 
-        for file in orphans:
-                # read all dependencies in manifest file and remove them from 'orphans' list 
-                # to ensure no package will lose functionality after orphan removal
-                open_file = open(file, "r")
-                deps = open_file.readlines()
+            for dep in deps:
+                try:
+                    new_orphans.remove(dep)
+                except:
+                    pass
 
-                for dep in deps:
-                        # remove '\n' from package name and add '.manifest' extension
-                        dep = (dep.split('\n'))[0]
-                        try:
-                                orphans.remove(dep + ".manifest")
-                        except:
-                                pass
-
-                # if file has dependencies, it is not an orphan.
-                if(deps != []):
-                        try:
-                                orphans.remove(file)
-                        except:
-                                pass
+            # if file has dependencies, it is not an orphan.
+            if(deps != []):
+                try:
+                    new_orphans.remove(file)
+                except:
+                    pass
 
         # resulting list contains only the packages that are not dependencies 
         # to other packages and that have no dependencies, which means: only orphan packages.
 
-        return orphans
+    print("Orphans: ", new_orphans)
+    return new_orphans
+
+remove_orphans()
