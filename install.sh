196a38a6ab01a6b5ed03bd628f590b7023e51bee
Matheus Artur Cabral Costa <macc@ic.ufal.br>
2019-04-09 22:24:24 -0300

    script test

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ mkdir ~/.mpu
 # copy all files
 cp * ~/.mpu -r
 # aliases
-cat .bashrc >> ~/.bashrc
-cat .bash_profile >> ~/.bash_profile
-#echo "alias mpu=\"python3 ~/.mpu/src/main.py\"">> ~/.bashrc
+#cat .bashrc >> ~/.bashrc
+#cat .bash_profile >> ~/.bash_profile
+echo "alias mpu=\"python3 ~/.mpu/src/main.py\"">> ~/.bashrc
 # reload bash
-bash -r
+. ~/.bashrc
 # important directories and files
 mkdir ~/.mpu/packages
 mkdir ~/.mpu/log
